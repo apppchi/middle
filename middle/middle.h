c3ce04b5f7df4f61147198995c9936ca0819efd9
apppchi <112154499+apppchi@users.noreply.github.com>
2022-10-24 07:26:28 +0300

    Add files via upload

--- middle/middle.h
+++ middle/middle.h
@@ -7,10 +7,12 @@ using namespace std;
 void itc_num_print(int number);
 int itc_len_num(long long number);
 int itc_sum_num(long long number);
-int itc_multi_num(long long number);
+long long itc_multi_num(long long number);
 int itc_max_num(long long number);
 int itc_min_num(long long number);
-
-
+int itc_rev_num(long long number);
+int itc_null_count(long long number);
+bool itc_mirror_num(long long number);
+int itc_mirror_count(long long number);
 
 #endif // MIDDLE_H_INCLUDED
